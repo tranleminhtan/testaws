abef16a778c01eda1d0d85a0d98857b072e2d2da
tranleminhtan <tranleminhtan@gmail.com>
2019-03-29 09:03:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,3 @@ res.sendFile(__dirname + "/demo.html");
 app.listen(port, function() {
 console.log('Listening on port ' + port)
 })
-
-//AKIARY26FYVXEHSZ57FB,+Vab4ItotQ5UQ1tKVtbLHzSwPf3ZSK8r8p30I1u0
